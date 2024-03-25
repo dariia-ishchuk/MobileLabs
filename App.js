47e3cd9c0b0ae63548d38cd40ff05abfd05b38bd
Dariia Ishchuk <ipz203_idv1@student.ztu.edu.ua>
2024-03-25 20:21:38 +0200

    Added header

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header
+          title="Лабораторна робота 1"
+          imageUrl={require('./assets/logo_zdtu.png')}
+      />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ components/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+const Header = ({ title, imageUrl }) => {
+    return (
+        <View style={styles.headerContainer}>
+            <Image source={imageUrl} style={styles.image} resizeMode="contain"/>
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>{title}</Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        paddingTop: 20,
+        paddingHorizontal: 20,
+        backgroundColor: '#f0f0f0',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        zIndex: 1000,
+    },
+    image: {
+        width: 150,
+        height: 50,
+        borderRadius: 25,
+        marginRight: 10,
+        marginTop: 15,
+        marginBottom: 10
+    },
+    titleContainer: {
+        flex: 1,
+        alignItems: 'flex-end',
+    },
+    title: {
+        fontSize: 17,
+        fontWeight: 'bold',
+    },
+});
+
+export default Header;
